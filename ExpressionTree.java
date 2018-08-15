a26f0eb00e85a0a8aea6b83d3f387d5f7ef92bee
persme11 <perfectspring11@gmail.com>
2018-08-15 19:33:51 -0400

    Add files via upload

---
+++ ExpressionTree.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.*;
+
+public class ExpressionTree{
+
+	private LinkedList l;
+	private String s;
+
+
+
+	ExpressionTree(String in){
+
+		this.s = in;
+
+	}
+		String[] CharArray = in.split('\\s*');
+
+
+	public int eval(){
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+}
